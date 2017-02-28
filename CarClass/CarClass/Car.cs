aa2b49785d0716dc4f5977f7705f44593862dbfa
Ianitsa <iani_georgieva@mail.bg>
2017-02-28 12:21:56 +0200

    Add files via upload

---
+++ CarClass/CarClass/Car.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    class Car
+    {
+        private string _Brand;
+        private string _Model;
+        //private decimal _Price;
+        //private double _Speed;
+        //private double _Power;
+        
+
+        public Car(string brand, string model,decimal price, DateTime BirthDate,double speed, double power, string fuel)
+        {
+            this.BirthDate = BirthDate;
+            this.Brand = brand;
+            this.Model = model;
+            this.Price = price;
+            this.Power = power;
+            this.Speed = speed;
+            this.Fuel = fuel;
+        }
+
+        public DateTime BirthDate { get; private set; }
+        public TimeSpan Age
+        {
+            get { return DateTime.Now - BirthDate; }
+        }
+        public string Brand
+        {
+            get {return this._Brand ;}
+            set
+            {
+                if (string.IsNullOrWhiteSpace(this._Brand))
+                {
+                    this._Brand = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException("Cannot rename a brand!");
+                };
+            }
+        }
+        public string Model
+        {
+            get { return this._Model; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(this._Model))
+                {
+                    this._Model = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException("Cannot rename a model!");
+                };
+            }
+        }
+        public decimal Price { get; set; }
+        public double Speed { get; set; }
+        public double Power { get; set; }
+        public string Fuel { get; private set; }
+
+
+
+    }
+}
---
+++ CarClass/CarClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("BMW", "750", 2000, new DateTime(2007, 5, 1), 300, 2000, "gasoline");
+
+            Console.WriteLine("{0} {1} - {6} has price {2} lv. It is {3} years. It has speed - {4}km/h  and it has {5} power", car.Brand, car.Model, car.Price, car.Age.Days / 365, car.Speed, car.Power, car.Fuel);
+        }
+    }
+}
