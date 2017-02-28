c1d389e2a60f3d38dc723df228884cf6e61b8c1b
Ianitsa <iani_georgieva@mail.bg>
2017-02-28 12:10:09 +0200

    Add files via upload

---
+++ Circles/Circles/Coordinates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circles
+{
+    class Coordinates
+
+    {
+        public Coordinates(double x, double y, double r)
+        {
+            this.X = x;
+            this.Y = y;
+            this.R = r;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double R { get; set; }
+
+    }
+}
---
+++ Circles/Circles/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x1, x2, y1, y2, r1, r2;
+            double circlesDistance;
+
+            Console.WriteLine("Enter the x1");
+            x1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the x2");
+            x2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the y1");
+            y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the y2");
+            y2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the r1");
+            r1 = double.Parse(Console.ReadLine());
+            if (r1<0)
+            {
+                Console.WriteLine("r1 must be > 0");
+                r1 = double.Parse(Console.ReadLine()); 
+            }
+            Console.WriteLine("Enter the r2");
+            r2 = double.Parse(Console.ReadLine());
+            if (r2 < 0)
+            {
+                Console.WriteLine("r2 must be > 0");
+                r2 = double.Parse(Console.ReadLine());
+            }
+
+            var circle1 = new Coordinates(x1,y1,r1);
+            var circle2 = new Coordinates(x2,y2,r2);
+
+            double distance = Math.Sqrt(Math.Pow(circle2.X, 2) - Math.Pow(circle1.X, 2));
+            circlesDistance= distance -(r2+r1);
+
+            if (circlesDistance<0)
+            {
+                //??
+                circlesDistance =Math.Abs( distance - (r2 -r1));
+            }
+            else if (circlesDistance==0)
+            {
+                circlesDistance = circle1.R - (distance + circle2.R);
+
+            }
+            Console.WriteLine("The distance between  the two circles is: {0}", circlesDistance);
+
+        }
+    }
+}
